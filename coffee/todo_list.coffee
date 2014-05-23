b5e77d5dc20208424cad68553319f69e8d8a9c1f
Shimpei Takamatsu <shimpeiws@gmail.com>
2014-05-23 16:33:38 +0900

    update TODO List

--- coffee/todo_list.coffee
+++ coffee/todo_list.coffee
@@ -1,17 +1,84 @@
 $ ->
-  setup_items = [
-    {name: 'Learn JavaScript'}
-    {name: 'Learn vue.js'}
-  ]
-  ko.applyBindings(new TodoModel(setup_items))
-  
-TodoModel = (items)->
-  _this = this
-  _this.page_title = 'todos'
-  _this.items = ko.observableArray(items)
-  this
-  
-  this.classChange = =>
-    console.log("classChange")
-  this
+  # ToDo一件あたりのオブジェクト
+  Todo = (title, done, order, callback)->
+    self = this
+    self.title = ko.observable(title)
+    self.done = ko.observable(done)
+    self.order = order
+    self.updateCallback = ko.computed  -> 
+        callback(self)
+        true
+      return
     
+    
+  # ViewModel
+  viewModel = ->
+    self = this
+    # HTML側でUpCaseできないので、ここは大文字
+    self.page_title = 'TODOS by Knockout.js'
+    self.todos = ko.observableArray([])
+    self.inputTitle = ko.observable("")
+    self.doneTodos = ko.observable(0)
+    self.markAll = ko.observable(false)
+    
+    self.addOne = ->
+      order = self.todos.length
+      newTodo = new Todo(self.inputTitle(), false, order, self.countUpdate)
+      self.todos.push(newTodo)
+      return
+      
+    self.createOnEnter = (item, event) ->
+      if event.keyCode == 13 && self.inputTitle()
+        # 新規のTodoオブジェクト作成 & Observerbleに入る
+        self.addOne()
+        #  Todo入力テキストボックスの初期化
+        self.inputTitle("")
+      else
+        return true
+      return
+      
+    # 編集中かどうかの(Enter押下時のため)
+    self.toggleEditMode = (item, event) ->
+      $(event.target).closest('li').toggleClass('editing')
+    
+    self.editOnEnter = (item, event) ->
+      if event.keyCode == 13 && item.title
+        item.updateCallback()
+        self.toggleEditMode(item, event)
+      else
+        return true
+      return
+      
+    self.markAll.subscribe((newValue) ->
+        ko.utils.arrayForEach(self.todos(), (it) ->
+            item.done(newValue)
+            return
+        )
+        return
+      )
+    
+    self.countUpdate = (item) ->
+      doneArray = ko.utils.arrayFilter(self.todos(), (it) ->
+          it.done()
+        )
+        self.doneTodos(doneArray.length)
+        return true
+      return
+    
+    self.countDoneText = (bool) ->
+      cntAll = self.todos().length
+      cnt = if bool then self.doneTodos() else cntAll - self.doneTodos()
+      text = "<span class='count'>" + cnt.toString() + "</span>"
+      text += if bool then " completed" else " remaining"
+      text += if self.doneTodos() > 1 then " items" else " item"
+      text
+      
+    self.clear = ->
+      self.todos.remove (item) ->
+          item.done()
+      return
+    return
+
+  ko.applyBindings(new viewModel())
+
+  return
--- js/todo_list.js
+++ js/todo_list.js
@@ -1,28 +1,79 @@
 (function() {
-  var TodoModel;
-
   $(function() {
-    var setup_items;
-    setup_items = [
-      {
-        name: 'Learn JavaScript'
-      }, {
-        name: 'Learn vue.js'
-      }
-    ];
-    return ko.applyBindings(new TodoModel(setup_items));
-  });
-
-  TodoModel = function(items) {
-    var _this = this;
-    _this = this;
-    _this.page_title = 'todos';
-    _this.items = ko.observableArray(items);
-    this;
-    this.classChange = function() {
-      return console.log("classChange");
+    var Todo, viewModel;
+    Todo = function(title, done, order, callback) {
+      var self;
+      self = this;
+      self.title = ko.observable(title);
+      self.done = ko.observable(done);
+      self.order = order;
+      self.updateCallback = ko.computed(function() {
+        callback(self);
+        return true;
+      });
     };
-    return this;
-  };
+    viewModel = function() {
+      var self;
+      self = this;
+      self.page_title = 'TODOS by Knockout.js';
+      self.todos = ko.observableArray([]);
+      self.inputTitle = ko.observable("");
+      self.doneTodos = ko.observable(0);
+      self.markAll = ko.observable(false);
+      self.addOne = function() {
+        var newTodo, order;
+        order = self.todos.length;
+        newTodo = new Todo(self.inputTitle(), false, order, self.countUpdate);
+        self.todos.push(newTodo);
+      };
+      self.createOnEnter = function(item, event) {
+        if (event.keyCode === 13 && self.inputTitle()) {
+          self.addOne();
+          self.inputTitle("");
+        } else {
+          return true;
+        }
+      };
+      self.toggleEditMode = function(item, event) {
+        return $(event.target).closest('li').toggleClass('editing');
+      };
+      self.editOnEnter = function(item, event) {
+        if (event.keyCode === 13 && item.title) {
+          item.updateCallback();
+          self.toggleEditMode(item, event);
+        } else {
+          return true;
+        }
+      };
+      self.markAll.subscribe(function(newValue) {
+        ko.utils.arrayForEach(self.todos(), function(it) {
+          item.done(newValue);
+        });
+      });
+      self.countUpdate = function(item) {
+        var doneArray;
+        doneArray = ko.utils.arrayFilter(self.todos(), function(it) {
+          return it.done();
+        });
+        self.doneTodos(doneArray.length);
+        return true;
+      };
+      self.countDoneText = function(bool) {
+        var cnt, cntAll, text;
+        cntAll = self.todos().length;
+        cnt = bool ? self.doneTodos() : cntAll - self.doneTodos();
+        text = "<span class='count'>" + cnt.toString() + "</span>";
+        text += bool ? " completed" : " remaining";
+        text += self.doneTodos() > 1 ? " items" : " item";
+        return text;
+      };
+      self.clear = function() {
+        self.todos.remove(function(item) {
+          return item.done();
+        });
+      };
+    };
+    ko.applyBindings(new viewModel());
+  });
 
 }).call(this);
